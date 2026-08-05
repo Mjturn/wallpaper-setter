6714b957a4065076262432cb86703c31a40b4bfc
Mjturn <Mjturn1@proton.me>
2026-08-05 18:16:59 -0400

    have randomized wallpaper persist on startup

--- script.sh
+++ script.sh
@@ -2,4 +2,6 @@
 
 filepath=~/Wallpapers
 wallpaper=$(find $filepath -type f | shuf -n 1)
+wallpaper=$(printf "%s\n" "$wallpaper" | sed "s|^$HOME|~|")
 hyprctl hyprpaper wallpaper "DVI-D-1, $wallpaper"
+sed -i "s|~.*|$wallpaper|" ~/.config/hypr/hyprpaper.conf
