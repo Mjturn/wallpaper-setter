5a560370b786c45cf557a58462a8496740321a2e
Mjturn <Mjturn1@proton.me>
2026-08-06 19:06:48 -0400

    change wallpaper directory and only support .jpg and .png files

--- script.sh
+++ script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-filepath=~/Wallpapers
-wallpaper=$(find $filepath -type f | shuf -n 1)
+filepath=~/Pictures/Wallpapers
+wallpaper=$(find $filepath -name "*.jpg" -o -name "*.png" -type f | shuf -n 1)
 wallpaper=$(printf "%s\n" "$wallpaper" | sed "s|^$HOME|~|")
 hyprctl hyprpaper wallpaper "DVI-D-1, $wallpaper"
 sed -i "s|~.*|$wallpaper|" ~/.config/hypr/hyprpaper.conf
